225f4ebb0e20eb2ec6eabce40f65ddf6c1458a2e
ZoltanVari <zoli.0843@gmail.com>
2025-07-16 21:12:28 +0200

    4

--- addon.ps1
+++ addon.ps1
@@ -4,12 +4,10 @@
 $GW2Path = "C:\Program Files\Guild Wars 2\"
 
 Remove-Item -Path "$($GW2Path)d3d11.dll" -Force
-Remove-Item -Path "$($GW2Path)arcdps_unofficial_extras.dll" -Force
 Remove-Item -Path "$($GW2Path)addons\arcdps\arcdps.ini" -Force
 
 New-Item -ItemType Directory -Force -Path "$($GW2Path)addons\arcdps\"
 Invoke-WebRequest "https://www.deltaconnected.com/arcdps/x64/d3d11.dll" -OutFile "$($GW2Path)d3d11.dll"
-Invoke-WebRequest "https://github.com/Krappa322/arcdps_unofficial_extras_releases/releases/latest/download/arcdps_unofficial_extras.dll" -OutFile "$($GW2Path)arcdps_unofficial_extras.dll"
 Invoke-WebRequest "https://raw.githubusercontent.com/xEcronomus/gw2arcdpsconfig/refs/heads/main/arcdps.ini" -OutFile "$($GW2Path)addons\arcdps\arcdps.ini"
 
 
